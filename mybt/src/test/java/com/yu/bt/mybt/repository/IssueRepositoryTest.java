b7bacb8d53466b1a1e8cfd9b5ad34a34491bdc93
disaster08 <merchkz@gmail.com>
2022-01-20 21:03:50 +0600

    refactoring tests

--- mybt/src/test/java/com/yu/bt/mybt/repository/IssueRepositoryTest.java
+++ mybt/src/test/java/com/yu/bt/mybt/repository/IssueRepositoryTest.java
@@ -34,15 +34,15 @@ class IssueRepositoryTest {
         userRepo.save(mike);
 
         Issue issue1 = new Issue( "something is wrong", "BUG", "high", "good description", "APPROVER");
-        issue1.setAssignee(2);
-        issue1.setReporter(1);
+        issue1.setAssignee(john.getId());
+        issue1.setReporter(mike.getId());
         this.issueRepo.save(issue1);
 
 
 
         Issue issue2 = new Issue("app is down", "INCIDENT", "high", "good description", "APPROVER");
-        issue2.setAssignee(1);
-        issue2.setReporter(2);
+        issue2.setAssignee(mike.getId());
+        issue2.setReporter(john.getId());
         this.issueRepo.save(issue2);
 
         //When
@@ -50,36 +50,36 @@ class IssueRepositoryTest {
 
         //Then
         assertThat(issues).isNotEmpty()
-        .hasSize(2)
-        .allMatch(issue -> issue.getDescription().equals("good description"));
+                .hasSize(2)
+                .allMatch(issue -> issue.getDescription().equals("good description"));
     }
 
     @Test
     void itShouldGetReportersIssuesById() {
 
         //given
-        User john = new User("john", "john@gmail.com", "123456");
-        userRepo.save(john);
+        User bob = new User("bob", "bob@gmail.com", "123456");
+        userRepo.save(bob);
 
-        User mike = new User("mike", "mike@gmail.com", "123456");
-        userRepo.save(mike);
+        User steve = new User("steve", "steve@gmail.com", "123456");
+        userRepo.save(steve);
 
         Issue issue1 = new Issue( "something is wrong", "BUG", "high", "good description", "APPROVER");
-        issue1.setAssignee(2);
-        issue1.setReporter(1);
+        issue1.setAssignee(bob.getId());
+        issue1.setReporter(steve.getId());
         this.issueRepo.save(issue1);
 
         Issue issue2 = new Issue("app is down", "INCIDENT", "high", "good description", "APPROVER");
-        issue2.setAssignee(1);
-        issue2.setReporter(2);
+        issue2.setAssignee(steve.getId());
+        issue2.setReporter(bob.getId());
         this.issueRepo.save(issue2);
 
         //When
-        List<Issue> issues = issueRepo.getReportersIssuesById(john.getId());
+        List<Issue> issues = issueRepo.getReportersIssuesById(bob.getId());
 
         //Then
         assertThat(issues).hasSize(1);
-        assertThat(issues.get(0).getSummary()).isEqualTo("something is wrong");
+        assertThat(issues.get(0).getSummary()).isEqualTo("app is down");
 
 
     }
@@ -88,28 +88,28 @@ class IssueRepositoryTest {
     void itShouldGetAssigneeIssuesById() {
 
         //given
-        User john = new User("john", "john@gmail.com", "123456");
-        userRepo.save(john);
+        User max = new User("max", "max@gmail.com", "123456");
+        userRepo.save(max);
 
-        User mike = new User("mike", "mike@gmail.com", "123456");
-        userRepo.save(mike);
+        User tom = new User("tom", "tom@gmail.com", "123456");
+        userRepo.save(tom);
 
         Issue issue1 = new Issue( "something is wrong", "BUG", "high", "good description", "APPROVER");
-        issue1.setAssignee(2);
-        issue1.setReporter(1);
+        issue1.setAssignee(max.getId());
+        issue1.setReporter(tom.getId());
         this.issueRepo.save(issue1);
 
         Issue issue2 = new Issue("app is down", "INCIDENT", "high", "good description", "APPROVER");
-        issue2.setAssignee(1);
-        issue2.setReporter(2);
+        issue2.setAssignee(tom.getId());
+        issue2.setReporter(max.getId());
         this.issueRepo.save(issue2);
 
         //When
-        List<Issue> issues = issueRepo.getAssigneeIssuesById(mike.getId());
+        List<Issue> issues = issueRepo.getAssigneeIssuesById(tom.getId());
 
         //Then
         assertThat(issues).hasSize(1);
-        assertThat(issues.get(0).getAssignee()).isEqualTo(2);
+        assertThat(issues.get(0).getAssignee()).isEqualTo(tom.getId());
 
     }
 }
--- mybt/src/test/java/com/yu/bt/mybt/services/IssueServiceTest.java
+++ mybt/src/test/java/com/yu/bt/mybt/services/IssueServiceTest.java
@@ -1,14 +1,14 @@
 package com.yu.bt.mybt.services;
 
+
 import com.yu.bt.mybt.models.dto.UsersIssuesDTO;
 import com.yu.bt.mybt.repository.CommentRepository;
 import com.yu.bt.mybt.repository.IssueRepository;
 import com.yu.bt.mybt.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 import java.util.List;
 
@@ -21,19 +21,22 @@ import static org.mockito.Mockito.when;
 class IssueServiceTest {
 
     @Test
-    void name() {
+    void getUsersIssues() {
+
         IssueRepository issues = mock(IssueRepository.class);
         UserRepository users = mock(UserRepository.class);
         CommentRepository comments = mock(CommentRepository.class);
-        when(issues.getReportersIssuesById(anyLong()))
-            .thenThrow(IllegalArgumentException.class);
 
         IssueService issueService = new IssueService(issues, users, comments);
 
         List<UsersIssuesDTO> usersIssues = issueService.getUsersIssues(1L);
-
-
         Assertions.assertTrue(usersIssues.isEmpty());
 
+
+    }
+
+    @Test
+    @Disabled
+    void getIssuesAndComments() {
     }
 }
